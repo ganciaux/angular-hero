022f6010b4d5cff538ea364f9bfc018e126e7a33
ganciaux <ghislain.anciaux@live.fr>
2026-05-22 15:55:22 +0200

    wip(form signal): update hero settings

--- src/app/features/hero/hero-settings/hero-settings.ts
+++ src/app/features/hero/hero-settings/hero-settings.ts
@@ -1,5 +1,8 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { HeroClass } from '../hero.model';
+import { form, max, min, minLength, required, FormField } from '@angular/forms/signals';
+import { HeroService } from '../hero.service';
+import { Router } from '@angular/router';
 
 interface HeroFormData {
   name: string;
@@ -9,20 +12,33 @@ interface HeroFormData {
 
 @Component({
   selector: 'app-hero-settings',
-  imports: [],
+  imports: [FormField],
   templateUrl: './hero-settings.html',
   styleUrl: './hero-settings.scss',
 })
 export class HeroSettings {
-heroModel = signal<HeroFormData>({name:'',heroClass:'warrior', level:1})
+  router = inject(Router)
+  heroService = inject(HeroService)
+  heroModel = signal<HeroFormData>({
+    name: this.heroService.hero().name,
+    heroClass: this.heroService.hero().heroClass,
+    level: this.heroService.hero().level,
+  });
+  
+  heroForm = form(this.heroModel, (path) => {
+    required(path.name, { message: 'Le nom est requis' });
+    minLength(path.name, 2, { message: 'Minimum 2 caractères' });
+    required(path.heroClass, { message: 'La classe est requise' });
+    min(path.level, 1, { message: 'Level minimum est 1' });
+    max(path.level, 5, { message: 'Level maximum est 5' });
+  });
 
-/*
-heroForm = form(this.heroModel, (path) => {
-  RequiredValidator(path.name, { message: 'Le nom est requis' });
-  minLength(path.name, 2, { message: 'Minimum 2 caractères' });
-  required(path.heroClass, { message: 'La classe est requise' });
-  min(path.level, 1, { message: 'Level minimum est 1' });
-  max(path.level, 5, { message: 'Level maximum est 5' });
-});
-*/
+  onSubmit(){
+    if (this.heroForm().valid()){
+      const {name, level, heroClass} = this.heroModel();
+      this.heroService.configure(name, heroClass, level)
+      this.router.navigate(['/hero']);
+    }
+  
+}
 }
